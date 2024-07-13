3bdaf7949da6c9a074252be1a9c2c6557ed0913c
GauravVKulkarni <92051349+GauravVKulkarni@users.noreply.github.com>
2024-07-13 23:32:44 +0530

    Day 1

---
+++ day-01.js
@@ -0,0 +1,51 @@
+//Activity 1:
+
+//task 1:
+var num = 100
+console.log(num)
+
+//task 2:
+let str = `Hello JS`
+console.log(str)
+
+
+//Activity 2:
+
+//task 3:
+const bool = true
+console.log(bool)
+
+
+//Activity 3:
+
+//task 4:
+const num1 = 10
+const str1 = `Hii`
+const bool1 = false
+const obj = new String(`This is string object`)
+const arr = [1, 2, 3, 4, 5]
+
+console.log(`${num1}: ${typeof num1}`)
+console.log(`${str1}: ${typeof str1}`)
+console.log(`${bool1}: ${typeof bool1}`)
+console.log(`${obj}: ${typeof obj}`)
+console.log(`${arr}: ${typeof arr}`)
+
+
+//Activity 4:
+
+//task 5:
+let variable = `first value`
+console.log(variable)
+
+variable = `second value`
+console.log(variable)
+
+
+//Activity 5:
+
+//task 6:
+const constant = `a constant string`
+constant = `changed string`
+
+console.log(constant)
